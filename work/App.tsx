46451139f5190c930f93456cc85469fa8327f8bb
Iago Leobas <iago.leobass@gmail.com>
2023-04-27 15:02:37 -0300

    feat: adding style

--- work/App.tsx
+++ work/App.tsx
@@ -1,11 +1,30 @@
-import { Fragment } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
+
+  //comentário
   return (
-    <Fragment>
-      <Text>Olá mundo!</Text>
-      <Text>Bom dia</Text>
-    </Fragment>
+    <View style={{
+      backgroundColor: '#131016',
+      flex: 1,
+      paddingHorizontal: 24,
+      paddingVertical: 25
+    }}>
+      <Text key={1}
+        style={{
+          color: '#FDFCFE',
+          fontSize: 24,
+          lineHeight: 28.13,
+          fontWeight: '700',
+        }}>
+        Nome do Evento
+      </Text>
+      <Text key={2}
+        style={{
+          color: '#6B6B6B',
+          lineHeight: 18.75,
+          fontWeight: "400",
+        }}>Sexta, 27 de Abril de 2023</Text>
+    </View>
   );
 }
