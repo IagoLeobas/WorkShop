cc022ba496f8ffd4cc1bcf2d3e69b7e7182fc541
Iago Leobas <iago.leobass@gmail.com>
2023-05-06 10:58:02 -0300

    feat: new updates

--- work/App.tsx
+++ work/App.tsx
@@ -1,16 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
   //comentário
   return (
     <View style={styled.constainer}>
-      <Text key={1} style={styled.text}>
+      <Text key={1} style={styled.title}>
         Nome do Evento
       </Text>
-      <Text key={2} style={styled.text2}>
+
+      <Text key={2} style={styled.subTitle}>
         Sexta, 27 de Abril de 2023
       </Text>
+
+      <View style={styled.containerInput}>
+        <TextInput
+          placeholder='Nome do Participante'
+          placeholderTextColor={'#6B6B6B'}
+          style={styled.input}
+        />
+
+        <TouchableOpacity style={styled.buttonAdd}>
+          <Text
+            style={styled.labelButton}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -23,16 +37,50 @@ const styled = StyleSheet.create({
     paddingVertical: 25
   },
 
-  text: {
+  title: {
     color: '#FDFCFE',
     fontSize: 24,
     lineHeight: 28.13,
     fontWeight: '700',
   },
 
-  text2: {
+  subTitle: {
     color: '#6B6B6B',
     lineHeight: 18.75,
+    fontSize: 16,
     fontWeight: "400",
-  }
+  },
+
+  input: {
+    flex: 1,
+    height: 56,
+    width: 317,
+    borderRadius: 4,
+    backgroundColor: '#1F1E25',
+    color: '#FDFCFE',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    fontSize: 14,
+  },
+
+  containerInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 34,
+    gap: 7,
+  },
+
+  buttonAdd: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 56,
+    width: 56,
+    borderRadius: 4,
+    backgroundColor: '#31CF67',
+  },
+
+  labelButton: {
+    color: '#fff',
+    fontSize: 24,
+  },
 })
